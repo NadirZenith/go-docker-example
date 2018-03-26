6daffaadbd80913645fb206bfd5594badc92c2bc
NadirZenith <2cb.md2@gmail.com>
2018-03-26 18:07:25 +0200

    added page links

--- wiki.go
+++ wiki.go
@@ -6,7 +6,8 @@ import (
 	"log"
 	"html/template"
 	"regexp"
-	"errors"
+	"fmt"
+	"golang.org/x/net/html"
 )
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
@@ -67,15 +68,6 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("Invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
-}
-
 func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Here we will extract the page title from the Request,
@@ -89,10 +81,57 @@ func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.Hand
 	}
 }
 
+// ---------------------------------------------------------------
+func pageLinksHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("q")
+	fmt.Fprintf(w, "Page = %q\n", url)
+	if len(url) == 0 {
+		return
+	}
+	page, err := parse("https://" + url)
+	if err != nil {
+		fmt.Printf("Error getting page %s %s\n", url, err)
+		return
+	}
+	links := pageLinks(nil, page)
+	for _, link := range links {
+		fmt.Fprintf(w, "Link = %q\n", link)
+	}
+}
+
+
+func parse(url string) (*html.Node, error) {
+	fmt.Println(url)
+	r, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot get page")
+	}
+	b, err := html.Parse(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse page")
+	}
+	return b, err
+}
+
+func pageLinks(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = pageLinks(links, c)
+	}
+	return links
+}
+
 func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/page-links", pageLinksHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -120,3 +159,4 @@ func main() {
 //	http.HandleFunc("/", handler)
 //	log.Fatal(http.ListenAndServe(":8080", nil))
 //}
+
